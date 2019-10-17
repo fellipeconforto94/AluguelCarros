4ca78ca5962294cc64347318e377a2e4bf655887
fellipeconforto94 <fellipeconforto94@gmail.com>
2019-10-17 12:44:35 -0300

    ProjetoEmAndamento

--- ProjetoLocacao/Program.cs
+++ ProjetoLocacao/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjetoLocacao.Model;
+using System;
 
 namespace ProjetoLocacao
 {
@@ -6,7 +7,10 @@ namespace ProjetoLocacao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TabelaPrecoModel tb = new TabelaPrecoModel()
+            {
+
+            };
         }
     }
 }
